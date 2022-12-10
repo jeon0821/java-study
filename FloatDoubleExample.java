7945264de06d1335eedb8feda9b7f4b703b02d41
jieun <plaintype@gmail.com>
2022-12-11 01:34:20 +0900

    실수값 저장

---
+++ FloatDoubleExample.java
@@ -0,0 +1,8 @@
+public class FloatDoubleExample{
+    public static void main(String[] args) {
+        //실수값 저장
+        //float var1 = 3.14; <- 컴파일 에러(Type mismatch)
+        float var2 = 3.14f;
+        double var3 = 3.14;
+    }
+}
