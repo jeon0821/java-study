d201ffbc1d0afae66ea6438b6ba87aad4f58ed8c
jieun <plaintype@gmail.com>
2022-12-11 01:28:50 +0900

    이스케이프 문자 출력 코드 예시

--- StringExample.java
+++ StringExample.java
@@ -13,6 +13,12 @@ public class StringExample {
 
         String str3 = "홍길동\n김자바";
         System.out.println(str3);
+
+        System.out.println("번호\t이름\t직업");
+        System.out.print("행 단위 출력\n");
+        System.out.print("행 단위 출력\n");
+        System.out.println("우리는 \"개발자\" 입니다.");
+        System.out.println("봄\\여름\\가을\\겨울");
     }
     
 }
