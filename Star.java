45320f4b493b13a63da711118cf045914dd1b577
jieun <plaintype@gmail.com>
2022-12-13 01:46:11 +0900

    별찍기 2번

--- Star.java
+++ Star.java
@@ -7,6 +7,15 @@ public class Star {
             }
             System.out.println();
         }
+
+        for(int i=0; i<6; i++){
+            for(int n=5; n>i; n--){
+                System.out.println(" ");
+            }
+            for(int j=0; j<i+1; j++){
+            System.out.println("*");
+            }
+            System.out.println();
+        }
+        }
     }
-    
-}
