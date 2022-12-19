c93c3a7140c666ab1d9e5a060fc494e000961b61
jieun <plaintype@gmail.com>
2022-12-19 23:44:22 +0900

    정수 타입의 연산 추가

--- ByteOperationExample.java
+++ ByteOperationExample.java
@@ -7,6 +7,13 @@ public class ByteOperationExample {
         byte y = 20;
         int result2 = x+y;
         System.out.println(result2);
+
+        // LongOperationExample
+        byte value1 = 10;
+        int value2 = 100;
+        long value3 = 1000L;
+        long result = value1 + value2 + value3;
+        System.out.println(result);
     }
     
 }
