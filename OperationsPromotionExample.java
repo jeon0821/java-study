cf861cae7b91d70f1b2637f79f1ffd1a16864ff4
jieun <plaintype@gmail.com>
2022-12-21 21:12:36 +0900

    연산식 자동 타입 변환 예시 2

--- OperationsPromotionExample.java
+++ OperationsPromotionExample.java
@@ -5,6 +5,14 @@ public class OperationsPromotionExample {
         //byte byteValue3 = byteValue1 + byteValue2; <- 컴파일 에러
         int intValue = byteValue1 + byteValue2;
         System.out.println(intValue1);
+
+        char charValue1 = 'A';
+        char charValue2 = 1;
+        //char charValue3 = charValue1 + charValue2; <- 컴파일 에러
+        int intValue2 = charValue1 + charValue2;
+        System.out.println("유니코드="+intValue2);
+        System.out.println("출력문자="+(char)intValue2);    
+    
     }
     
 }
