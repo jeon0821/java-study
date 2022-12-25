f3af8298bb95354022e3fb6b3f0b5d9cf261659e
jieun <plaintype@gmail.com>
2022-12-25 23:41:16 +0900

    String 홍길동

--- PrintfExample.java
+++ PrintfExample.java
@@ -8,6 +8,10 @@ public class PrintfExample {
 
         double area = 3.14159 * 10 * 10;
         System.out.println("반지름이 %d인 원의 넓이:%10.2f\n", 10, area);
+
+        String name = "홍길동";
+        String job = "도적";
+        System.out.println("%6d | %-10s | %10s\n", 1, name, job);
     }
     
 }
